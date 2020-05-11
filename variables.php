dcf4172a8072ab9cc2e32ddc5898cb46ebb05b1e
lrodrigro <64587195+lrodrigro@users.noreply.github.com>
2020-05-11 10:19:30 +0200

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php
+
+$filmName = 'Indiana Jones et la dernière croisade';  //string
+$filmView = true;                                     //boolean
+$filmYear = 1989;                                     //integer
+$filmRate = 8.2;                                      //float
+
+echo $filmName . PHP_EOL;
+echo $filmView . "\r\n";
+echo $filmYear . PHP_EOL;
+echo $filmRate . PHP_EOL;
